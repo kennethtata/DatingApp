aa348e61b2c19a2a012262b92ecc0820f8d17267
Ken Curtis <kcurtis@quickspark.com>
2022-08-22 11:47:11 -0700

    message done

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -34,7 +34,10 @@ namespace API.Data
 
         public async Task<Message> GetMessage(int id)
         {
-            return await _context.Messages.FindAsync(id);
+            return await _context.Messages
+                .Include(u => u.Sender)
+                .Include(u => u.Recipient)
+                .SingleOrDefaultAsync(x => x.Id == id);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -45,9 +48,9 @@ namespace API.Data
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.UserName),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.UserName),
-                _ => query.Where(u => u.Recipient.UserName == messageParams.UserName && u.DateRead == null)
+                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.UserName && u.RecipientDeleted == false),
+                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.UserName && u.SenderDeleted == false),
+                _ => query.Where(u => u.Recipient.UserName == messageParams.UserName && u.RecipientDeleted == false && u.DateRead == null)
             };
 
             var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
@@ -61,12 +64,13 @@ namespace API.Data
 
             //get the conversation of users
             var messages = await _context.Messages
+
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
                 .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-                .Where(m => m.Recipient.UserName == currentUserName
+                .Where(m => m.Recipient.UserName == currentUserName && m.RecipientDeleted == false
                 && m.Sender.UserName == recipientUserName
                 || m.Recipient.UserName == recipientUserName
-                && m.Sender.UserName == currentUserName
+                && m.Sender.UserName == currentUserName && m.SenderDeleted == false
                 )
                 .OrderBy(m => m.MessageSent)
                 .ToListAsync();
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -11,9 +11,10 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
 messages: Message[] = [];
 pagination: Pagination;
-container = 'Inbox';
+container = 'Unread';
 pageNumber = 1;
 pageSize = 5;
+loading = false;
 
   constructor(private messageService: MessageService) { }
 
@@ -23,10 +24,19 @@ pageSize = 5;
 
 
   loadMessages(){
+    this.loading = true;
     this.messageService.getMessage(this.pageNumber,this.pageSize,this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
+      this.loading = false;
 
+    })
+  }
+
+  deleteMessage(id: number)
+  {
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
     })
   }
 
