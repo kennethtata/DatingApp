0e8395f5d8b15ef08be7a9a213f41fc7a42ecb06
kennethtata <kenncurtis@hotmail.com>
2023-01-04 00:32:02 -0700

    add live stream to back end

---
+++ client/src/app/_services/livestream.service.ts
@@ -0,0 +1,98 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
+import { Message } from '../_models/message';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { User } from '../_models/user';
+import { BehaviorSubject, take,Subject } from 'rxjs';
+import { Group } from '../_models/group';
+import { BusyService } from './busy.service';
+import { UserInfo, SignalInfo } from 'src/app/_models/peerData.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivestreamService {
+  baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
+
+  //tutorialcode
+  private newPeer = new Subject<User>();
+  public newPeer$ = this.newPeer.asObservable();
+
+  private helloAnswer = new Subject<User>();
+  public helloAnswer$ = this.helloAnswer.asObservable();
+
+  private disconnectedPeer = new Subject<User>();
+  public disconnectedPeer$ = this.disconnectedPeer.asObservable();
+
+  private signal = new Subject<SignalInfo>();
+  public signal$ = this.signal.asObservable();
+
+  constructor(private http: HttpClient, private busyService: BusyService) {}
+
+  createHubConnection(user: User, otherUserName: string) {
+    this.busyService.busy();
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + 'streaming?user=' + otherUserName, {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection
+      .start()
+      .catch((error) => console.log(error))
+      .finally(() => this.busyService.idle());
+
+    this.hubConnection.on('ReceiveMessageThread', (messages) => {
+      this.messageThreadSource.next(messages);
+    });
+
+    this.hubConnection.on('UpdatedGroup', (group: Group) => {
+      if (group.connections.some((x) => x.userName === otherUserName)) {
+        this.messageThread$.pipe(take(1)).subscribe((messages) => {
+          messages.forEach((message) => {
+            if (!message.dateRead) {
+              message.dateRead = new Date(Date.now());
+            }
+          });
+          this.messageThreadSource.next([...messages]);
+        });
+      }
+    });
+
+    //tutorialcode
+    this.hubConnection.on('NewUserArrived', (data) => {
+      this.newPeer.next(JSON.parse(data));
+      console.log('NewUserArrived' + JSON.parse(data));
+    });
+
+    this.hubConnection.on('UserSaidHello', (data) => {
+      this.helloAnswer.next(JSON.parse(data));
+      console.log('UserSaidHello' + JSON.parse(data));
+    });
+
+    this.hubConnection.on('UserDisconnect', (data) => {
+      this.disconnectedPeer.next(JSON.parse(data));
+    });
+
+    this.hubConnection.on('SendSignal', (user, signal) => {
+      this.signal.next({ user, signal });
+    });
+
+    this.hubConnection.invoke('NewUser', otherUserName);
+  }
+
+  public sendSignalToUser(signal: string, user: string) {
+    this.hubConnection.invoke('SendSignal', signal, user);
+  }
+
+  public sayHello(userName: string, user: string): void {
+    this.hubConnection.invoke('HelloUser', userName, user);
+  }
+}
